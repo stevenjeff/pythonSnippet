03f4862bbcbf0652aa8371822ed0a85e8bf30631
fangruizhang <stevenjeff@126.com>
2019-12-18 00:06:21 +0800

    evergrande

--- PyDemo/PyDemo/org/fangrui/Evergrande.py
+++ PyDemo/PyDemo/org/fangrui/Evergrande.py
@@ -7,13 +7,19 @@ rootPath = "D:\\360Downloads\\evergrande\\"
 excelName = "3、建安-新 - 副本 - 副本.xlsx"
 excelNameReName = "3、建安-新 - 副本 - 副本_done.xlsx"
 projectName = "3.建安图片"
+# 汇总excel范围
+sumRange = "A3:Y652"
+# 合并明细excel范围
+detailRange = "A2:U2381"
 projectRoot = rootPath + projectName
 linkColumn = 22
 labelColumn = 24
 valueColumn = "L"
+contractNoIndex = 3
+# 明细合同列
+contractNoIndexDetail = 111
 totalSheetName = "汇总"
 detailsheetName = "合并明细（带合同号）"
-range = "A3:Y652"
 ifVisible = False
 voucherDir = "凭证"
 notContractvoucherDir = "无合同"
@@ -27,20 +33,22 @@ def excelProccessXlwings():
     # 保存
     totalSheet = workbook.sheets[totalSheetName]
     detailSheet = workbook.sheets[detailsheetName]
-    rng = totalSheet.range(range)
+    rng = totalSheet.range(sumRange)
     currentCompanyIndex = 0
     for row in rng.rows:
         if row[0].value is not None:
             currentCompanyIndex = int(row[0].value)
-        if row[3].value is None or row[2].value is None or row[11].value is None or row[3].value == "无合同" or row[
+        if row[contractNoIndex].value is None or row[2].value is None or row[11].value is None or row[
+            contractNoIndex].value == "无合同" or row[
             2].value == "无合同" or str(
-                row[3].value).strip() == "" or str(row[2].value).strip() == "":
+            row[contractNoIndex].value).strip() == "" or str(row[2].value).strip() == "":
             row[labelColumn].value = "无合同"
             continue
         companyNameDir = getCompanyDir(currentCompanyIndex)
         if companyNameDir is None:
             continue
-        currentCompanyDir = projectRoot + "\\" + companyNameDir
+        contractMoneyDir = row[3].value + "--" + row[2].value + "--" + str(row[11].value)
+        copyFiles(companyNameDir, row[contractNoIndex], contractMoneyDir, detailSheet)
         row[linkColumn].formula = '=HYPERLINK(".\\' + projectName + '\\' + companyNameDir + '",' + valueColumn + str(
             row.row) + ')'
         print(getCompanyDir(currentCompanyIndex))
@@ -50,6 +58,22 @@ def excelProccessXlwings():
     app.quit()
     print("end:", time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
 
+
+def copyFiles(companyNameDir, contractNo, contractMoneyDir, detailSheet):
+    currentCompanyDir = projectRoot + "\\" + companyNameDir + "\\"
+    contractDir = currentCompanyDir + companyNameDir.split("、")[1].strip()
+    contractMoneyDirFullPath = projectRoot + "\\" + companyNameDir + "\\" + contractMoneyDir
+    os.mkdir(contractMoneyDirFullPath)
+    folderlist = os.listdir(contractDir)  # 列举文件夹
+    for currentDir in folderlist:
+        dircontractName = contractNoReplace(currentDir)
+        contractName = contractNoReplace(contractNo)
+        if contractName in dircontractName:
+            shutil.copytree()
+
+
+def contractNoReplace(contractNo):
+    return contractNo.replace("[", "").replace("]", "").replace("【", "").replace("】", "")
 
 def walkDir():
     folderlist = os.listdir(projectRoot)  # 列举文件夹
